a463e2e14aed6d33ce78ca2abab7d60c3747b696
Josh Glantz-Hucks <jahteo@gmail.com>
2020-09-15 18:11:23 -0700

    axios get successful, & displaying friends

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 export default function FriendsList () {
 
+  const [friends, setFriends] = useState([])
+
+  const getFriends = () => {
+    axiosWithAuth()
+      .get("/api/friends")
+      .then(res => {
+        console.log("getFriends res.data:", res.data)
+        setFriends(res.data)
+      })
+      .catch(err => {
+        console.log("getFriends err:", err)
+      })
+  }
+
+  useEffect(() => {
+    getFriends()
+  }, [])
+
+  const formatData = () => {
+    //Needed???
+  }
+
   return (
-    <div>FriendsList component says hi</div>
+    <div>FriendsList component says hi
+      {friends.map(friend => {
+        return (
+          <div>
+            <h2>{friend.name}</h2>
+            {/* this would be a cool spot to hover/flip */}
+            {friend.age} <br />
+            {friend.email}
+
+          </div>
+        )
+      })}
+
+    </div>
   )
 }
